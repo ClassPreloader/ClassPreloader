49bcd3785f558214c6078bf1740fd81fc4c34558
Adrien Brault <adrien.brault@gmail.com>
2013-03-01 11:32:51 +0100

    Fix binary autoload require

--- classpreloader.php
+++ classpreloader.php
@@ -1,6 +1,10 @@
 #! /usr/bin/env php
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloadPath = __DIR__ . '/../../autoload.php')) {
+    require_once $autoloadPath;
+} else {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
 $application = new ClassPreloader\Application();
 $application->run();
