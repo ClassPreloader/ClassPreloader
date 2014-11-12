5f44b677c8b98decfeff03ff57c6a0be7799aab7
Martin Keckeis <martin.keckeis1@gmail.com>
2014-11-12 09:39:07 +0100

    Update CommandTest.php

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -21,7 +21,18 @@ class CommandTest extends PHPUnit_Framework_TestCase
 - Writing $bar
 - Writing $foo
 > Compiled loader written to $out
-- 0 kb
+- Files: 2/2 (skipped: 0)
+- Filesize: 0 kb
+EOT;
+        $expectedSkip = <<<EOT
+> Loading configuration file
+- Found 1 files
+> Compiling classes
+- Skipping $bar
+- Skipping $foo
+> Compiled loader written to $out
+- Files: 0/2 (skipped: 2)
+- Filesize: 0 kb
 EOT;
 
         $first = <<<EOT
@@ -146,7 +157,7 @@ EOT;
                 $expected,
                 $third,
             ),
-                        array(
+            array(
                 array(
                     '--config'   => __DIR__ . DIRECTORY_SEPARATOR . 'classlist.php',
                     '--output'   => $out,
@@ -154,6 +165,15 @@ EOT;
                 ),
                 $expected,
                 $last,
+            ),
+            array(
+                array(
+                    '--config'   => __DIR__ . DIRECTORY_SEPARATOR . 'classlist.php',
+                    '--output'   => $out,
+                    '--skip_dir_file' => true,
+                ),
+                $expectedSkip,
+                '<?php',
             ),
         );
     }
