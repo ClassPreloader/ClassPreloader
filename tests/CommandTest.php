aea5839d7572b7f5c3e10914b3e7fc27f04d0f14
Graham Campbell <graham@mineuk.com>
2014-11-12 22:49:49 +0000

    Added a missing newline

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -201,6 +201,7 @@ EOT;
         $string = str_replace("\r\n", "\n", $string);
         $string = str_replace("\r", "\n", $string);
         $string = preg_replace("/\n{2,}/", "\n\n", $string);
+
         return rtrim($string);
     }
 }
