af73fe39fc937a16f6750a17dcb51bf414b38d13
Graham Campbell <graham@mineuk.com>
2015-05-17 23:29:35 +0100

    Import IteratorAggregate

--- src/Config.php
+++ src/Config.php
@@ -13,13 +13,14 @@
 namespace ClassPreloader;
 
 use ClassPreloader\Parser\AbstractNodeVisitor;
+use IteratorAggregate;
 
 /**
  * This is the config class.
  *
  * This contains all the class preloader configuration.
  */
-class Config implements \IteratorAggregate
+class Config implements IteratorAggregate
 {
     /**
      * The array of AbstractNodeVisitor objects that visit nodes.
