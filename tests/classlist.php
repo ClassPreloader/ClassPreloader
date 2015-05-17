f2c779f37f94bfc9d93dee5177fd0f3d021553b5
Graham Campbell <graham@mineuk.com>
2015-05-17 23:28:47 +0100

    Added copyright information to the files

--- tests/classlist.php
+++ tests/classlist.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of Class Preloader.
+ *
+ * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Michael Dowling <mtdowling@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return ClassPreloader\ClassLoader::getIncludes(function (ClassPreloader\ClassLoader $loader) {
     $loader->register();
     new Foo();
