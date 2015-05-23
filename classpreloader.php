c4e3a8d61be502bd04e7cdcdc2c80d7916e15fab
Graham Campbell <graham@cachethq.io>
2015-05-23 14:43:47 +0100

    Updated email address

--- classpreloader.php
+++ classpreloader.php
@@ -4,7 +4,7 @@
 /*
  * This file is part of Class Preloader.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  * (c) Michael Dowling <mtdowling@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
